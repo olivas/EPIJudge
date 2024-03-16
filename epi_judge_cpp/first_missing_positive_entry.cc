4c73d3f5f833c7eb9d12f5f97c952a8d03fad71f
Alex Olivas <alex.r.olivas@gmail.com>
2024-03-16 13:19:44 -0600

    doesn't pass yet, but i'm close.

--- epi_judge_cpp/first_missing_positive_entry.cc
+++ epi_judge_cpp/first_missing_positive_entry.cc
@@ -1,11 +1,30 @@
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 #include "test_framework/generic_test.h"
 using std::vector;
 
 // A is passed by value argument, since we change it.
 int FindFirstMissingPositive(vector<int> A) {
-  // TODO - you fill in here.
+  // 1) sort the array
+  // 2) iterate and track the diff between this and last
+  //    if diff > 1 then return last + 1
+  // the running time is dominated by the sort - O(NlogN)
+  // the second loop is O(N)
+  std::sort(std::begin(A), std::end(A));
+
+  int last{*(A.begin())};
+  for(auto iter = A.begin()++; iter != A.end(); ++iter){
+      std::cout<<"("<<last<<" "<<*iter<<")"<<std::endl;
+      int diff = *iter - last;
+      if(*iter > 0 && diff > 1){
+          if(last <= 0)
+              return 1;
+          return last + 1;
+      }
+      last = *iter;
+  }
   return 0;
 }
 
